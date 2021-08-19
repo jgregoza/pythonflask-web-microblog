a04b88749e6f43234852d29fbde6e3bb5383815b
Gregorio Zamora <86702253+jgregoza@users.noreply.github.com>
2021-08-19 12:37:58 -0600

    Microblog  - Python
    Sitio creado con Python-Flask-Mongodb-HTML5-CSS..

---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+import datetime
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+import certifi
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+client = MongoClient(os.environ.get("MONGODB_URI"))
+db = client.microblog
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        db.entries.insert_one({"content": entry_content, "date": formatted_date})
+
+    entries_with_date = [
+        (
+        entry["content"],
+        entry["date"],
+        datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+        )
+        for entry in db.entries.find({})
+    ]
+    return render_template("home.html", entries=entries_with_date)
+
+if __name__ == '__main__':
+    app.run(debug=True)
